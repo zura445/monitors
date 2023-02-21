81d1255ee23184dcdcb10119c926f45f033d46ea
DESKTOP-O4E0STK\Rac Gagixardeba <zuratetra@gmail.com>
2023-02-21 18:19:26 +0400

    styles.ts

--- pages/homePage/Header.tsx
+++ pages/homePage/Header.tsx
@@ -1,14 +1,15 @@
-import { HomeDiv } from "./styles";
+import Typography from "../views/components/typography";
+import { HomeDiv, Contact } from "./styles";
 
 const Header = () => {
     return(
-        <HomeDiv className="flex justify-around">
-            <img src="image/logo.png" className="w-10 h-10" alt="" />
-            <div className="">tgrhtgvbhjnm</div>
-            <div className="">gfhbjnmk,l</div>
-            <div className="">tvgbhjnmkl.;</div>
-            <div className="">retryctvbyhnjmyfvgbh</div>
-            
+        <HomeDiv className="flex justify-around bg-current py-2">
+            <img src="image/logo.png" className="w-10 h-10 rounded-full" alt="" />
+            <Typography type={"body"} color={"dark"} className="text-white cursor-pointer">Products</Typography>
+            <Typography type={"body"} color={"dark"} className="text-white cursor-pointer">UserCases</Typography>           
+            <Typography type={"body"} color={"dark"} className="text-white cursor-pointer">Who We Serve</Typography>
+            <Typography type={"body"} color={"dark"} className="text-white cursor-pointer">About</Typography>            
+            <Contact>Contact Us</Contact>         
         </HomeDiv>
     )
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import Header from './homePage/Header'
-// import Head from 'next/head'
-// import Image from 'next/image'
-import Typography from './views/components/typography'
+
 
 const Home: NextPage = () => {
   return (
